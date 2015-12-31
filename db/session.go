e9a42f6fdd0e68b5dba6c2b1bf850e7015b2d2b3
Pompeu <itacir@hotmail.com>
2015-12-30 23:21:48 -0200

    css and db refacotry

--- db/session.go
+++ db/session.go
@@ -1,8 +1,50 @@
 package db
 
 import (
+	"errors"
 	"gopkg.in/mgo.v2"
 )
+
+type MongoDb struct {
+	mongoUrl string
+	database string
+	session  *mgo.Session
+}
+
+func NewMongoConnect(mongoUrl, database string) (m MongoDb, e error) {
+	m.mongoUrl = mongoUrl
+	m.database = database
+	session, err := mgo.Dial(m.mongoUrl)
+	if err != nil {
+		return m, mongoerr(err.Error())
+	}
+	if err := session.Ping(); err != nil {
+		return m, mongoerr(err.Error())
+	}
+	index := setIndex([]string{"email"})
+	if err := session.DB(m.database).C("test").EnsureIndex(index); err != nil {
+		return m, mongoerr(err.Error())
+	}
+	m.session = session
+	return
+}
+
+func setIndex(key []string) mgo.Index {
+	return mgo.Index{
+		Key:    key,
+		Unique: true,
+	}
+}
+
+func mongoerr(msg string) error {
+	return errors.New("mongodb: " + msg)
+}
+
+func (b MongoDb) Close() {
+	if b.session != nil {
+		b.session.Close()
+	}
+}
 
 func Session(col string) *mgo.Collection {
 	session, err := mgo.Dial("mongodb://localhost/" + col)
@@ -10,4 +52,12 @@ func Session(col string) *mgo.Collection {
 		panic(err)
 	}
 	return session.DB("test").C(col)
+}
+
+func SimpleSession(col string) *mgo.Session {
+	session, err := mgo.Dial("mongodb://localhost/" + col)
+	if err != nil {
+		panic(err)
+	}
+	return session
 }
